97d12c5db24948c9eb31b95b9e50dd131ad71201
Chanidapaka <chanidapapanta@gmail.com>
2025-01-21 11:42:50 +0700

    Add Code02

---
+++ 210168/Practice/Practice01.js
@@ -0,0 +1,56 @@
+// Problem 1 Write a function to reverse a string. The function should return the reversed string
+//Example: reverseString("JavaScript")  --> "tpircSavaJ"
+
+/* แนวคิด
+เอามาต่อกัน คือ ("J"+"a")
+อ่านจากด้านหน้ามาด้านหลัง คือ (Javascript"[0])
+อ่านจากด้านหลัง คือ (Javascript"["Javascript".lenght])
+
+ */
+
+function reverseString(str) {
+          let  reverse = '' 
+          
+}
+
+//Problem 02
+//Example:
+
+/*แนวคิด อ่านตัวแรกมาเก็บไว้ แล้วshift และ put เข้าใหม่
+  */
+ let fruits = ['Apple', 'Banana', 'Cherry', 'Orange']
+ //fruits.shift() //ลบตัวแรก
+ fruits.splice(0,1) 
+ console.log(fruits)
+
+ //Problem 03
+ //Example:
+
+/*แนวคิด  อ่านตัวแรกมาเก็บไว้ แล้วshift และ put เข้าใหม่ เขียนfor loop
+  */
+
+//Problem 04
+ //Example:
+
+/*แนวคิด  merge คือ รวม2 Array และผลลัพธ์เป็น Array ใหม่ เป็น for loop 2 ครั้ง 
+อ่าน Array = [1,2,3] และเอาค่านั้นมาput ลงArray อีกตัวนึง
+*/
+
+//Problem 05
+ //Example:
+
+/*แนวคิด  merge คือ รวม2 Array และผลลัพธ์เป็น Array ใหม่ 
+1.เช็คตวามยาวของทั้ง2อัน 
+2.ใช้loop เดียว เอาตัวที่มีความยาวมากกว่า loop
+3.ใช้if เช็คเงื่อนไข 
+*/
+
+//Problem 06
+ //Example:
+
+/*แนวคิด  
+*/
+
+
+
+
