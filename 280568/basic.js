ad9eee82c6cf6c6cc84fc36ad17af1394238b8e0
Chanidapaka <chanidapapanta@gmail.com>
2025-05-28 23:52:51 +0700

    28/05/2568

--- 280568/basic.js
+++ 280568/basic.js
@@ -90,19 +90,93 @@
 // replace(/[^a-z0-9]/g)
 
 
-function sumEvenNumbers(numbers) {
-          let newNum = 0
+// function sumEvenNumbers(numbers) {
+//           let newNum = 0
 
-          for (let i = 0; i < numbers.length; i++) {
-                    const element = numbers[i];
+//           for (let i = 0; i < numbers.length; i++) {
+//                     const element = numbers[i];
 
-                    if (element % 2 === 0) {
-                              newNum += element
+//                     if (element % 2 === 0) {
+//                               newNum += element
+//                     }
+                    
+//           }
+//           return newNum
+// }
+// console.log(sumEvenNumbers([1, 2, 3, 4, 5, 6])) 
+// console.log(sumEvenNumbers([10, -3, 8, 0, -1, 7])) 
+// console.log(sumEvenNumbers([1, 3, 5, 7])) 
+
+//1
+function findMaxNum(arr) {
+
+          let max = arr[0]
+
+          for (let i = 1; i < arr.length; i++) {
+                    const currentNumber = arr[i];
+
+                    if (currentNumber > max) {
+                              max = currentNumber
                     }
                     
           }
-          return newNum
+          return max
+          
 }
-console.log(sumEvenNumbers([1, 2, 3, 4, 5, 6])) 
-console.log(sumEvenNumbers([10, -3, 8, 0, -1, 7])) 
-console.log(sumEvenNumbers([1, 3, 5, 7])) 
+console.log(findMaxNum([3,9,1,22,5])) // should return 22
+
+//2
+function filterOdd(num) {
+          let newNum = []
+
+          for (let i = 0; i < num.length; i++) {
+                    const element = num[i];
+                    
+                    if (element % 2 !== 0) {
+                          newNum.push(element)  
+                    }
+          }
+          return newNum
+          
+}
+console.log(filterOdd([1, 2, 3, 4, 5, 6]))
+
+//3
+function removeDuplicates(arr) {
+  let result = [];
+  for (let i = 0; i < arr.length; i++) {
+    let exists = false;
+    for (let j = 0; j < result.length; j++) {
+      if (arr[i] === result[j]) {
+        exists = true;
+        break;
+      }
+    }
+    if (!exists) {
+      result.push(arr[i]);
+    }
+  }
+  return result;
+}
+console.log(removeDuplicates([1, 2, 2, 3, 4, 4, 5]))
+
+
+//4
+function sumArray(arr) {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum;
+}
+console.log(sumArray([1, 2, 3, 4]))
+
+//5
+function reverseArray(arr) {
+  let result = [];
+  for (let i = arr.length - 1; i >= 0; i--) {
+    result.push(arr[i]);
+  }
+  return result;
+}
+console.log(reverseArray([1, 2, 3]))
